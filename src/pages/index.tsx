35eec135fce91380f795b830f3bb479374f07e3b
brunoBrian <bruninho_brian@hotmail.com>
2023-04-03 21:30:57 -0300

    add eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '@styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -15,8 +15,6 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -67,5 +65,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
