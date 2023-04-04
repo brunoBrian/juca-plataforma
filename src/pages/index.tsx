4e7c94404c6309d0875b8f301ccda20dfb9afe75
brunoBrian <bruninho_brian@hotmail.com>
2023-04-03 21:25:00 -0300

    configuring directories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '@styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -15,6 +15,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+
 
         <p className={styles.description}>
           Get started by editing{' '}
