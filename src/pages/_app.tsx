efd3a9242c499c02a6bf0c4abf176863732b15d7
brunoBrian <bruno.brian@vemprojuca.com>
2023-04-05 21:29:42 -0300

    add react-grid-system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { globalStyles } from '@styles/global';
+import { setConfiguration } from 'react-grid-system';
 
 globalStyles();
+setConfiguration({ gutterWidth: 20, maxScreenClass: 'xl' });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -72,6 +72,14 @@ export const {
       small: '0.5rem', //8px
       medium: '1rem', //16px
       circular: '50%'
+    },
+    media: {
+      sm: '(min-width: 576px)',
+      md: '(min-width: 768px)',
+      lg: '(min-width: 992px)',
+      xl: '(min-width: 1200px)',
+      xxl: '(min-width: 1600px)',
+      xxxl: '(min-width: 1920px)'
     }
   }
 });
