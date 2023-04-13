d5597f481a5782180f685495634371c879ad0c98
brunoBrian <bruno.brian@vemprojuca.com>
2023-04-13 17:34:46 -0300

    recarga-celular components directory adjust

---
+++ src/components/pages/RecargaCelular/index.tsx
@@ -0,0 +1,45 @@
+import { useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import { PaymentPaymentInfoLabelText } from '@components/Layout/Payment/components';
+import { Payment } from '@components/Layout';
+import { RechargeAmount, RechargePhoneData } from './components';
+
+export function RecargaCelular() {
+  const [step, setStep] = useState(0);
+  const router = useRouter();
+
+  const handleNextStep = useCallback(() => {
+    setStep(stp => (stp === 2 ? stp : stp + 1));
+  }, []);
+
+  const handlePreviousStep = useCallback(() => {
+    setStep(stp => (stp === 0 ? stp : stp - 1));
+  }, []);
+
+  const stepComponents = [
+    <RechargePhoneData
+      key={0}
+      nextStep={handleNextStep}
+      prevStep={() => router.push('/produtos')}
+    />,
+    <RechargeAmount
+      key={1}
+      nextStep={handleNextStep}
+      prevStep={handlePreviousStep}
+    />,
+    <Payment key={2}>
+      <PaymentPaymentInfoLabelText
+        label="Nome/Razão social:"
+        description="Claro Net Telecomunicações S.A"
+      />
+      <PaymentPaymentInfoLabelText
+        label="Valor da recarga:"
+        description="R$ 00,00"
+        descriptionBig
+      />
+    </Payment>
+  ];
+
+  return <>{stepComponents[step]}</>;
+}
--- src/pages/recarga-celular.tsx
+++ src/pages/recarga-celular.tsx
@@ -2,48 +2,10 @@ import Head from 'next/head';
 import { parseCookies } from 'nookies';
 import { GetServerSideProps } from 'next';
 
-import { Payment, RechargeAmount, RechargePhoneData } from '@components/index';
 import { Layout } from '@components/Layout/Layout/layout';
-import { PaymentPaymentInfoLabelText } from '@components/Layout/Payment/components';
-import { useCallback, useState } from 'react';
-import { useRouter } from 'next/router';
+import { RecargaCelular } from '@components/pages/RecargaCelular';
 
 export default function RecargaCelularPage() {
-  const [step, setStep] = useState(0);
-  const router = useRouter();
-
-  const handleNextStep = useCallback(() => {
-    setStep(stp => (stp === 2 ? stp : stp + 1));
-  }, []);
-
-  const handlePreviousStep = useCallback(() => {
-    setStep(stp => (stp === 0 ? stp : stp - 1));
-  }, []);
-
-  const stepComponents = [
-    <RechargePhoneData
-      key={0}
-      nextStep={handleNextStep}
-      prevStep={() => router.push('/produtos')}
-    />,
-    <RechargeAmount
-      key={1}
-      nextStep={handleNextStep}
-      prevStep={handlePreviousStep}
-    />,
-    <Payment key={2}>
-      <PaymentPaymentInfoLabelText
-        label="Nome/Razão social:"
-        description="Claro Net Telecomunicações S.A"
-      />
-      <PaymentPaymentInfoLabelText
-        label="Valor da recarga:"
-        description="R$ 00,00"
-        descriptionBig
-      />
-    </Payment>
-  ];
-
   return (
     <>
       <Head>
@@ -54,7 +16,7 @@ export default function RecargaCelularPage() {
         />
       </Head>
 
-      {stepComponents[step]}
+      <RecargaCelular />
     </>
   );
 }
